1fa7dea0508261fa8e2c4558afde705aea2cc2f8
Yuthika Anvitha <159745395+YuthikaAnvitha@users.noreply.github.com>
2025-04-14 15:48:42 +0530

    Update auth.dart
    added dependencies in comments

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 
+//environment:
+//   sdk: ">=2.17.0 <4.0.0"
+//
+// dependencies:
+//   flutter:
+//     sdk: flutter
+//   mad:
+//     git:
+//       url: https://github.com/sudhiksha-22/MAD.git
+//
+//   # The following adds the Cupertino Icons font to your application.
+//   # Use with the CupertinoIcons class for iOS style icons.
+//   cupertino_icons: ^1.0.8
+//   firebase_core: ^3.12.1
+//   firebase_auth: ^5.5.1
+//   cloud_firestore: ^5.6.5
+//   firebase_analytics: ^11.4.4
+//   flutter_rating_bar: ^4.0.1
+//   http: ^0.13.6
+//   intl: ^0.18.0
+
 class AuthScreen extends StatefulWidget {
   @override
   _AuthScreenState createState() => _AuthScreenState();
